b65e00e466d7124407edd2f382a4ed4da7a405ef
Lauren Budorick <lauren@mapbox.com>
2016-05-25 19:05:07 -0400

    Building shadow colors

--- src/building.vertex.glsl
+++ src/building.vertex.glsl
@@ -11,6 +11,7 @@ attribute vec3 a_normal;
 uniform mat4 u_matrix;
 uniform vec4 u_color;
 uniform vec3 u_lightdir;
+uniform vec4 u_shadow;
 varying vec4 v_color;
 
 void main() {
@@ -21,10 +22,10 @@ void main() {
     // TODO better var names for these numbers
     float t = mod(a_normal.x, 2.0);
     float directional = clamp(dot(a_normal / 32768.0, u_lightdir), 0.0, 1.0);
-    float b = clamp((0.3 - directional) / 7.0, 0.0, 0.3);
+    float shadow = clamp((0.3 - directional) / 7.0, 0.0, 0.3);
     directional = mix(0.7, 1.0, directional * 2.0 * (0.2 + t) / 1.2);
 
     v_color.rgb *= directional;
-    v_color.b += b;
+    v_color += shadow * u_shadow;
 }
 
