9d78072df46a374a7a519e73c1b3bb55fa904390
Lauren Budorick <lauren@mapbox.com>
2016-06-13 18:11:36 -0700

    dds color, opacity

--- src/extrusion.fragment.glsl
+++ src/extrusion.fragment.glsl
@@ -9,9 +9,14 @@ precision mediump float;
 varying vec4 v_color;
 #pragma mapbox: define lowp float minH
 #pragma mapbox: define lowp float maxH
+#pragma mapbox: define lowp vec4 color
+#pragma mapbox: define highp float opacity
 
 void main() {
     #pragma mapbox: initialize lowp float minH
     #pragma mapbox: initialize lowp float maxH
+    #pragma mapbox: initialize lowp vec4 color
+    #pragma mapbox: initialize highp float opacity
+
     gl_FragColor = v_color;
 }
