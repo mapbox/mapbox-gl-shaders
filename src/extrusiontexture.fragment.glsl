c556b70460484943b9cd95789880b5d7ac68e5a0
Lauren Budorick <lauren@mapbox.com>
2016-09-19 15:48:48 -0700

    Remove unused code/comments

--- src/extrusiontexture.fragment.glsl
+++ src/extrusiontexture.fragment.glsl
@@ -11,18 +11,6 @@ uniform float u_opacity;
 
 varying vec2 v_pos;
 
-// TODO here we'll probably want to add ability to fade in...
-// #ifndef MAPBOX_GL_JS
-// varying vec2 v_pos;
-// #else
-// uniform float u_opacity0;
-// uniform float u_opacity1;
-// uniform sampler2D u_image0;
-// uniform sampler2D u_image1;
-// varying vec2 v_pos0;
-// varying vec2 v_pos1;
-// #endif
-
 void main() {
     gl_FragColor = texture2D(u_texture, v_pos) * u_opacity;
 }
